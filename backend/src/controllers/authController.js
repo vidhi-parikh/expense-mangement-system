68bc7eae318ec60632a00905e2f6d0e1dfa9929b
vidhi-parikh <parikhvidhi10@gmail.com>
2024-06-01 16:19:06 +0530

    implement user login

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -20,3 +20,26 @@ exports.register = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body)
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      logger.warn("Invalid login attempt", { email });
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    logger.info("User logged in successfully", { email });
+    res.status(200).json({ token });
+  } catch (err) {
+    logger.error("Error logging in user", {
+      error: err.message,
+      stack: err.stack,
+    });
+    res.status(500).json({ error: "Server error" });
+  }
+};
