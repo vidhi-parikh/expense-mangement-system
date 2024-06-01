21bf4566865a5fed334731d970206623233c9bfc
vidhi-parikh <parikhvidhi10@gmail.com>
2024-06-01 14:48:52 +0530

    chore: integrated logger service

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./services/db_service');
 const dotenv = require('dotenv');
+const logger = require('./services/logger');
 
 dotenv.config();
 connectDB();
@@ -8,4 +9,4 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
